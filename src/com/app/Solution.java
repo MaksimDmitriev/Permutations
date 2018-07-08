06c23569c5e51d5af570fabdbf3e0884257820b6
Maksim Dmitriev <maksim.a.dmitriev@gmail.com>
2018-07-08 11:54:14 +0300

    Copy the solution from https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/

--- src/com/app/Solution.java
+++ src/com/app/Solution.java
@@ -1,14 +1,46 @@
 package com.app;
 
-import java.util.HashSet;
 import java.util.Set;
 
 public class Solution {
 
-    public Set<String> permutations(String input) {
-        final Set<String> result = new HashSet<>();
-        result.add("12");
-        result.add("21");
-        return result;
+    /**
+     * permutation function
+     *
+     * @param str string to calculate permutation for
+     * @param l   starting index
+     * @param r   end index
+     */
+    private void permutationFinder(String str, int l, int r, Set<String> result) {
+        if (l == r) {
+            result.add(str);
+        } else {
+            for (int i = l; i <= r; i++) {
+                str = swap(str, l, i);
+                permutationFinder(str, l + 1, r, result);
+                // str = swap(str, l, i);
+            }
+        }
+    }
+
+    public void permutationFinder(String str, Set<String> result) {
+        permutationFinder(str, 0, str.length() - 1, result);
+    }
+
+    /**
+     * Swap Characters at position
+     *
+     * @param a string value
+     * @param i position 1
+     * @param j position 2
+     * @return swapped string
+     */
+    private String swap(String a, int i, int j) {
+        char temp;
+        char[] charArray = a.toCharArray();
+        temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+        return String.valueOf(charArray);
     }
 }
--- test/com/app/SolutionTest.java
+++ test/com/app/SolutionTest.java
@@ -6,79 +6,97 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolutionTest {
 
     @Test
     void empty() {
         Solution solution = new Solution();
-        assertEquals(Collections.EMPTY_SET, solution.permutations(""));
+
+        Set<String> result = new HashSet<>();
+        solution.permutationFinder("", result);
+
+        assertEquals(Collections.EMPTY_SET, result);
     }
 
     @Test
     void single() {
         Solution solution = new Solution();
+        Set<String> expected = new HashSet<>();
+        expected.add("1");
+
         Set<String> result = new HashSet<>();
-        result.add("1");
-        assertEquals(result, solution.permutations("1"));
+        solution.permutationFinder("1", result);
+
+        assertEquals(expected, result);
     }
 
     @Test
     void twoSymbols() {
         Solution solution = new Solution();
+        Set<String> expected = new HashSet<>();
+        expected.add("12");
+        expected.add("21");
+
         Set<String> result = new HashSet<>();
-        result.add("12");
-        result.add("21");
-        assertEquals(result, solution.permutations("12"));
+        solution.permutationFinder("12", result);
+
+        assertEquals(expected, result);
     }
 
     @Test
     void threeSymbols() {
         Solution solution = new Solution();
+        Set<String> expected = new HashSet<>();
+        expected.add("123");
+        expected.add("132");
+        expected.add("213");
+        expected.add("231");
+        expected.add("312");
+        expected.add("321");
+
         Set<String> result = new HashSet<>();
-        result.add("123");
-        result.add("132");
-        result.add("213");
-        result.add("231");
-        result.add("312");
-        result.add("321");
-        assertEquals(result, solution.permutations("123"));
+        solution.permutationFinder("123", result);
+
+        assertEquals(expected, result);
     }
 
     @Test
     void fourSymbols() {
         Solution solution = new Solution();
+        Set<String> expected = new HashSet<>();
+        expected.add("1234");
+        expected.add("1243");
+        expected.add("1324");
+        expected.add("1342");
+        expected.add("1423");
+        expected.add("1432");
+
+        expected.add("2134");
+        expected.add("2143");
+        expected.add("2314");
+        expected.add("2341");
+        expected.add("2413");
+        expected.add("2431");
+
+        expected.add("3124");
+        expected.add("3142");
+        expected.add("3214");
+        expected.add("3241");
+        expected.add("3412");
+        expected.add("3421");
+
+        expected.add("4123");
+        expected.add("4132");
+        expected.add("4213");
+        expected.add("4231");
+        expected.add("4312");
+        expected.add("4321");
+
         Set<String> result = new HashSet<>();
-        result.add("1234");
+        solution.permutationFinder("1234", result);
 
-        result.add("1243");
-        result.add("1324");
-        result.add("1342");
-        result.add("1423");
-        result.add("1432");
-
-        result.add("2134");
-        result.add("2143");
-        result.add("2314");
-        result.add("2341");
-        result.add("2413");
-        result.add("2431");
-
-        result.add("3124");
-        result.add("3142");
-        result.add("3214");
-        result.add("3241");
-        result.add("3412");
-        result.add("3421");
-
-        result.add("4123");
-        result.add("4132");
-        result.add("4213");
-        result.add("4231");
-        result.add("4312");
-        result.add("4321");
-
-        assertEquals(result, solution.permutations("1234"));
+        assertEquals(expected, result);
     }
 }
